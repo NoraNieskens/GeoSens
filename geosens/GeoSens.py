66deb90c758ad4a575003baa0ed4ea4deef1227a
Nora Nieskens <57681769+NoraNieskens@users.noreply.github.com>
2021-08-25 10:39:11 +0200

    Add files via upload
    new icon 
    some functionalities added

--- geosens/GeoSens.py
+++ geosens/GeoSens.py
@@ -11,7 +11,6 @@
         copyright            : (C) 2021 by Nora Nieskens, University of Würzburg
         email                : nora.nieskens@stud-mail.uni-wuerzburg.de
  ***************************************************************************/
-
 /***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -23,7 +22,7 @@
 """
 from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
 from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import QAction
+from qgis.PyQt.QtWidgets import QAction, QFileDialog
 
 # Initialize Qt resources from file resources.py
 from .resources import *
@@ -37,7 +36,6 @@ class GeoSens:
 
     def __init__(self, iface):
         """Constructor.
-
         :param iface: An interface instance that will be passed to this class
             which provides the hook by which you can manipulate the QGIS
             application at run time.
@@ -70,12 +68,9 @@ class GeoSens:
     # noinspection PyMethodMayBeStatic
     def tr(self, message):
         """Get the translation for a string using Qt translation API.
-
         We implement this ourselves since we do not inherit QObject.
-
         :param message: String for translation.
         :type message: str, QString
-
         :returns: Translated version of message.
         :rtype: QString
         """
@@ -95,39 +90,29 @@ class GeoSens:
         whats_this=None,
         parent=None):
         """Add a toolbar icon to the toolbar.
-
         :param icon_path: Path to the icon for this action. Can be a resource
             path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
         :type icon_path: str
-
         :param text: Text that should be shown in menu items for this action.
         :type text: str
-
         :param callback: Function to be called when the action is triggered.
         :type callback: function
-
         :param enabled_flag: A flag indicating if the action should be enabled
             by default. Defaults to True.
         :type enabled_flag: bool
-
         :param add_to_menu: Flag indicating whether the action should also
             be added to the menu. Defaults to True.
         :type add_to_menu: bool
-
         :param add_to_toolbar: Flag indicating whether the action should also
             be added to the toolbar. Defaults to True.
         :type add_to_toolbar: bool
-
         :param status_tip: Optional text to show in a popup when mouse pointer
             hovers over the action.
         :type status_tip: str
-
         :param parent: Parent widget for the new action. Defaults None.
         :type parent: QWidget
-
         :param whats_this: Optional text to show in the status bar when the
             mouse pointer hovers over the action.
-
         :returns: The action that was created. Note that the action is also
             added to self.actions list.
         :rtype: QAction
@@ -178,6 +163,13 @@ class GeoSens:
                 self.tr(u'&GeoSens'),
                 action)
             self.iface.removeToolBarIcon(action)
+            
+    
+            
+    def select_output_file(self):
+        filename, _filter = QFileDialog.getSaveFileName(
+          self.dlg, "Select   output file ","", '*.tif')
+        self.dlg.lineEdit_3.setText(filename)
 
 
     def run(self):
@@ -188,6 +180,7 @@ class GeoSens:
         if self.first_start == True:
             self.first_start = False
             self.dlg = GeoSensDialog()
+            self.dlg.pushButton.clicked.connect(self.select_output_file)
 
         # show the dialog
         self.dlg.show()
